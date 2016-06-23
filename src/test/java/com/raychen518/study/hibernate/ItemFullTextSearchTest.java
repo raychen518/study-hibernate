55a40922f02f9f61023b4a759c67d50faf723003
rchen <ray.chen@flatironssolutions.com>
2016-06-23 17:56:19 +0800

    Add the configuration settings for log4j and HSQLDB.

---
+++ src/test/java/com/raychen518/study/hibernate/ItemFullTextSearchTest.java
@@ -0,0 +1,18 @@
+package com.raychen518.study.hibernate;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ItemFullTextSearchTest {
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @Test
+    public void test() {
+
+    }
+
+}
