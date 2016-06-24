ea6a79e74360ca2f4274f881aad2f8449b241fc3
rchen <ray.chen@flatironssolutions.com>
2016-06-24 14:18:31 +0800

    Add the Event Manager class.

--- src/main/java/com/raychen518/study/hibernate/HibernateUtil.java
+++ src/main/java/com/raychen518/study/hibernate/HibernateUtil.java
@@ -1,26 +1,28 @@
 package com.raychen518.study.hibernate;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
 
-	private static final SessionFactory sessionFactory = buildSessionFactory();
+    private static final SessionFactory sessionFactory = buildSessionFactory();
 
-	private static SessionFactory buildSessionFactory() {
-		try {
-			// Create the SessionFactory from hibernate.cfg.xml
-			return new Configuration().configure().buildSessionFactory(new StandardServiceRegistryBuilder().build());
-		} catch (Throwable ex) {
-			// Make sure you log the exception, as it might be swallowed
-			System.err.println("Initial SessionFactory creation failed." + ex);
-			throw new ExceptionInInitializerError(ex);
-		}
-	}
+    private static SessionFactory buildSessionFactory() {
+        try {
+            StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+            return new MetadataSources(registry).buildMetadata().buildSessionFactory();
 
-	public static SessionFactory getSessionFactory() {
-		return sessionFactory;
-	}
+        } catch (Throwable ex) {
+            // StandardServiceRegistryBuilder.destroy(registry);
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
 
 }
